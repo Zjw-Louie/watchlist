36d86a501dd67d3073eb486b899200a55b9cb99f
Louie <1565957931@qq.com>
2022-03-05 00:08:53 +0800

    add .gitignore and app.py

--- app.py
+++ app.py
@@ -1,32 +1,44 @@
-from flask import Flask, render_template
-# from flask import url_for
-app = Flask(__name__)
+import os
+import sys
+import click
 
-# @app.route('/home')
-# def hello():
-#     return 'Hello'
-#
-# @app.route('/user/<name>')
-# def user_page(name):
-#     return 'User: %s' % name
-#
-# @app.route('/test')
-# def test_url_for():
-#     # 下面是一些调试用例(请在命令行窗口查看输出的URL)
-#     print(url_for('index')) #输出:/
-#     #注意下面两个调用是如何生成包含 URL 变量的 URL 的
-#     print(url_for('user_page',name='greyli'))
-#
-#     print(url_for('user_page',name='peter'))
-#
-#     print(url_for('test_url_for'))
-#
-#     print(url_for('test_url_for',num=2))
-#     return 'Test page'
+
+from flask import Flask,render_template
+from flask_sqlalchemy import SQLAlchemy #导入扩展类
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+ os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # 关闭对模型 修改的监控
+# 在扩展类实例化前加载配置
+db = SQLAlchemy(app)  # 初始化扩展，传入程序实例 app
+class User(db.Model): # 表名将会是 user（自动生成，小写处理）
+    id = db.Column(db.Integer, primary_key=True) # 主键
+    name = db.Column(db.String(20)) # 名字
+
+class Movie(db.Model): # 表名将会是 movie
+    id = db.Column(db.Integer, primary_key=True) # 主键
+    title = db.Column(db.String(60)) # 电影标题
+    year = db.Column(db.String(4)) # 电影年份
 
 @app.route('/')
 def index():
+    user = User.query.first() # 读取用户记录 
+    movies = Movie.query.all() # 读取所有电影记录
     return render_template('index.html', name=name, movies=movies)
+@app.cli.command() # 注册为命令 
+def forge(): 
+    """Generate fake data.""" 
+    db.create_all()
+@app.cli.command() # 注册为命令 
+@click.option('--drop', is_flag=True, help='Create after drop.') # 设置选项 
+def initdb(drop): 
+    """Initialize the database.""" 
+    if drop: # 判断是否输入了选项 
+        db.drop_all() 
+    db.create_all() 
+    click.echo('Initialized database.') # 输出提示信息
+
 
 name = 'Grey Li'
 movies = [
@@ -41,3 +53,13 @@ movies = [
     {'title': 'WALL-E', 'year': '2008'},
     {'title': 'The Pork of Music', 'year': '2012'},
 ]
+user = User(name=name) 
+db.session.add(user) 
+for m in movies: 
+    movie = Movie(title=m['title'], year=m['year']) 
+    db.session.add(movie) 
+
+db.session.commit() 
+click.echo('Done.')
+
+
